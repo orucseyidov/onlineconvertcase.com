e81da1b759a72b695fa25b91374f38163057c092
orucseyidov <seyidovoruc@gmail.com>
2023-10-04 18:48:41 +0400

    jsonltd

--- application/views/json-ltd/breadcrumb.php
+++ application/views/json-ltd/breadcrumb.php
@@ -6,12 +6,12 @@
     "@type": "ListItem", 
     "position": 1, 
     "name": "Home",
-    "item": "/"  
+    "item": "<?=base_url() ?>"  
   },{
     "@type": "ListItem", 
     "position": 2, 
     "name": "<?=$breadcrumbTitle ?? $title ?>",
-    "item": "<?=$this->uri->segment(2) ?>"  
+    "item": "<?=base_url($this->uri->segment(2)) ?>"  
   }]
 }
 </script>
