fe1329a7d24488cb7bff10aeba2b71f34efa6ee3
orucseyidov <seyidovoruc@gmail.com>
2023-10-04 22:01:40 +0400

    a

--- application/views/json-ltd/tool.php
+++ application/views/json-ltd/tool.php
@@ -15,7 +15,7 @@
       foreach ($other_tool_json as $key => $value): 
         $tool_line .= '{"@type": "HowToTool","name": "'.$value['title'].'"},';  
       endforeach; 
-      echo rtirim(rtrim($tool_line,','),"}");
+      echo rtrim(rtrim($tool_line,','),"}");
       ?>
     ],
   <?php endif ?>
