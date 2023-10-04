424afc4dec844e196b9df96abffcdd41a9c183e7
orucseyidov <seyidovoruc@gmail.com>
2023-10-04 22:06:28 +0400

    a

--- application/views/home/otherTools.php
+++ application/views/home/otherTools.php
@@ -6,12 +6,14 @@
           <div class="other-tool-title">
             <h3><?=$value['title'] ?></h3>
           </div>
-          <div class="other-tool-links">
+          <div class="other-tool-links" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
             <?php 
               if(isset($value['tools']) && is_array($value['tools'])):
                 foreach ($value['tools'] as $toolsKey => $toolsValue):
             ?>
-              <a class="other-link-btn" href="<?=base_url("en/{$toolsValue['slug']}") ?>"><?=$toolsValue['title'] ?></a>
+              <a itemprop="url" class="other-link-btn" href="<?=base_url("en/{$toolsValue['slug']}") ?>">
+                <span itemprop="name"><?=$toolsValue['title'] ?></span>
+              </a>
             <?php 
                 endforeach;
               endif;
