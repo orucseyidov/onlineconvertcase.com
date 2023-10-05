3c7273e945b50aafa9f052783b010dbd1881eff9
orucseyidov <seyidovoruc@gmail.com>
2023-10-05 19:43:34 +0400

    a

--- application/views/blocks/header.php
+++ application/views/blocks/header.php
@@ -8,7 +8,7 @@
                       <div class="col-12 menu-area">
                           <div class="menu-and-logo">
                             <div class="logo-desktop">
-                                <a href="<?=base_url($locale) ?>">
+                                <a href="<?=base_url() ?>">
                                   <img alt="Convert case" src="/assets/img/logo-dark.png">
                                 </a>
                             </div>
