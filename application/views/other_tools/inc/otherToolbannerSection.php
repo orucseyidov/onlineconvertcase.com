b4af62d2b8a11b6d648ccaebaa1ebd47091eb952
orucseyidov <seyidovoruc@gmail.com>
2023-10-19 23:17:28 +0400

    last update

--- application/views/other_tools/inc/otherToolbannerSection.php
+++ application/views/other_tools/inc/otherToolbannerSection.php
@@ -10,7 +10,7 @@
           </div>
           <div class="banner-text">
               <div>
-                <h1><?=$tool['seo_title'] ?></h1>
+                <h1><?=$tool['seo_title'] ?> aasasa</h1>
                 <?=$tool['description'] ?>
               </div>
           </div>
