18e4a2a1c7b2ceefb481159c970f9700157a80e4
orucseyidov <seyidovoruc@gmail.com>
2023-10-20 14:53:03 +0400

    A

--- application/views/home/other_tools/index.php
+++ application/views/home/other_tools/index.php
@@ -3,5 +3,8 @@ $this->load->view("home/other_tools/otherToolbannerSection");
 $this->load->view("home/textCase");
 $this->load->view("home/compairButtons");
 $this->load->view("home/other_tools/about-tool");
+if (isset($other_tools) && is_array($other_tools) && count($other_tools)) {
+  $this->load->view("home/otherTools");
+}
 $this->load->view("home/feedback");
 ?>
