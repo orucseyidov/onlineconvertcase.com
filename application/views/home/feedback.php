7e42327e70cf3e23bf6dbab00538ea48228bfe5d
orucseyidov <seyidovoruc@gmail.com>
2023-10-25 20:14:54 +0400

    a

--- application/views/home/feedback.php
+++ application/views/home/feedback.php
@@ -12,13 +12,13 @@
                   <input class="feedback-input" type="checkbox" name="rate" value="1">
                   <div>
                       <svg class="eye left">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="eye right">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="mouth">
-                          <use xlink:href="#mouth">
+                          <use xlink:href="#mouth"></use>
                       </svg>
                   </div>
               </li>
@@ -26,13 +26,13 @@
                 <input class="feedback-input" type="checkbox" name="rate" value="2">
                   <div>
                       <svg class="eye left">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="eye right">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="mouth">
-                          <use xlink:href="#mouth">
+                          <use xlink:href="#mouth"></use>
                       </svg>
                   </div>
               </li>
@@ -44,13 +44,13 @@
                 <input class="feedback-input" type="checkbox" name="rate" value="4" checked>
                   <div>
                       <svg class="eye left">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="eye right">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="mouth">
-                          <use xlink:href="#mouth">
+                          <use xlink:href="#mouth"></use>
                       </svg>
                   </div>
               </li>
@@ -58,14 +58,14 @@
                 <input class="feedback-input" type="checkbox" name="rate" value="5">
                   <div>
                       <svg class="eye left">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                       <svg class="eye right">
-                          <use xlink:href="#eye">
+                          <use xlink:href="#eye"></use>
                       </svg>
                   </div>
               </li>
-          </ul>
+            </ul>
           <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
               <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 4" id="eye">
                   <path d="M1,1 C1.83333333,2.16666667 2.66666667,2.75 3.5,2.75 C4.33333333,2.75 5.16666667,2.16666667 6,1"></path>
