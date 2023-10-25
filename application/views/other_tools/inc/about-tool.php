df9c721ea871015335e94e98e5ec7565753da08e
orucseyidov <seyidovoruc@gmail.com>
2023-10-25 20:19:35 +0400

    a

--- application/views/other_tools/inc/about-tool.php
+++ application/views/other_tools/inc/about-tool.php
@@ -8,9 +8,9 @@
           <div class="card">
             <div class="card-body">
               <h2 class="card-title"><?$tool['seo_title'] ?></h2>
-              <p class="card-text">
+              <div class="card-text">
                 <?=$tool['seo_description'] ?>
-              </p>
+              </div>
             </div>
           </div>
         </div>
